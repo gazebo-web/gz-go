b067558f2382e431aed3147ee84dd0d1f485f5a2
Marcos Huck <marcoshuck@ekumenlabs.com>
2020-01-14 11:24:11 -0300

    Add coverage file creation

--- coverage.sh
+++ coverage.sh
@@ -6,10 +6,11 @@ echo "mode: atomic" > coverage.tx
 
 PKG_LIST=$(go list ./... | grep -v /vendor/)
 for package in ${PKG_LIST}; do
-    go test -covermode=atomic -coverprofile "coverage/${package##*/}.out" "$package" ;
-    if [ -f "coverage/${package##*/}.out" ]; then
-        echo "$(pwd)"
-        cat "coverage/${package##*/}.out" | grep -v "mode: " >> coverage.tx
-        rm "coverage/${package##*/}.out"
-    fi
+  touch "./coverage/${package##*/}.out"
+  go test -covermode=atomic -coverprofile "./coverage/${package##*/}.out" "$package" ;
+  if [ -f "./coverage/${package##*/}.out" ]; then
+      echo "$(pwd)"
+      cat "./coverage/${package##*/}.out" | grep -v "mode: " >> coverage.tx
+      rm "./coverage/${package##*/}.out"
+  fi
 done
