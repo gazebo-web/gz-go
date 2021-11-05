ede634f2380f1a956070aa5fa2dc9190c1fac275
Marcos Huck <marcoshuck@ekumenlabs.com>
2021-11-05 17:30:15 -0300

    Add HTTP client implementation

---
+++ io/http.go
@@ -0,0 +1,88 @@
+package io
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"gitlab.com/ignitionrobotics/web/ign-go/encoders"
+	"io"
+	"net/http"
+	"net/url"
+	"reflect"
+)
+
+// httpClient is an implementation of Client using an HTTP transport layer.
+type httpClient struct {
+	// client is the underlying HTTP client.
+	client *http.Client
+	// baseURL is the base URL where all the requests should be routed to.
+	baseURL *url.URL
+}
+
+// Call calls a method to a given endpoint using an HTTP
+func (c *httpClient) Call(ctx context.Context, method, endpoint string, format encoders.Format, in, out encoders.Serializer) error {
+	if in == nil || out == nil {
+		return ErrNilValuesIO
+	}
+	if reflect.ValueOf(out).Kind() != reflect.Ptr {
+		return ErrOutputMustBePointer
+	}
+
+	var err error
+	switch format {
+	case encoders.FormatJSON:
+		err = c.callJSON(ctx, method, endpoint, in, out)
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// callJSON is a helper function of Call used for JSON-specific HTTP requests.
+func (c *httpClient) callJSON(ctx context.Context, method, endpoint string, in, out encoders.JSON) error {
+	body, err := in.ToJSON()
+	if err != nil {
+		return err
+	}
+	buff := bytes.NewBuffer(body)
+
+	u, err := c.baseURL.Parse(endpoint)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequestWithContext(ctx, method, u.String(), buff)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	body, err = io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		return errors.New(string(body))
+	}
+
+	if err = out.FromJSON(body); err != nil {
+		return err
+	}
+	return nil
+}
+
+// NewClientHTTP initializes a new Client implementation using HTTP as transport layer.
+func NewClientHTTP(opts ClientOptions) Client {
+	return &httpClient{
+		client: &http.Client{
+			Timeout: opts.Timeout,
+		},
+		baseURL: opts.URL,
+	}
+}
---
+++ io/http_test.go
@@ -0,0 +1,109 @@
+package io
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/ignitionrobotics/web/ign-go/encoders"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+)
+
+type inputTest struct {
+	Data string `json:"data"`
+}
+
+func (in *inputTest) ToJSON() ([]byte, error) {
+	return json.Marshal(in)
+}
+
+func (in *inputTest) FromJSON(data []byte) error {
+	return json.Unmarshal(data, in)
+}
+
+type outputTest struct {
+	Result string `json:"result"`
+}
+
+func (out *outputTest) ToJSON() ([]byte, error) {
+	return json.Marshal(out)
+}
+
+func (out *outputTest) FromJSON(data []byte) error {
+	return json.Unmarshal(data, out)
+}
+
+func TestHTTPClient_CallWithIOErrors(t *testing.T) {
+	u, err := url.Parse("http://localhost")
+	require.NoError(t, err)
+
+	c := NewClientHTTP(ClientOptions{
+		Timeout: 10 * time.Second,
+		URL:     u,
+	})
+
+	err = c.Call(context.Background(), http.MethodPost, "/test", encoders.FormatJSON, nil, nil)
+	assert.Error(t, err)
+	assert.Equal(t, ErrNilValuesIO, err)
+
+	err = c.Call(context.Background(), http.MethodPost, "/test", encoders.FormatJSON, &inputTest{}, nil)
+	assert.Error(t, err)
+	assert.Equal(t, ErrNilValuesIO, err)
+
+	err = c.Call(context.Background(), http.MethodPost, "/test", encoders.FormatJSON, nil, &outputTest{})
+	assert.Error(t, err)
+	assert.Equal(t, ErrNilValuesIO, err)
+}
+
+func TestHttpClient_Call(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var in inputTest
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if err = in.FromJSON(body); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		out := outputTest{Result: in.Data}
+
+		body, err = out.ToJSON()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = w.Write(body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	u, err := url.ParseRequestURI(server.URL)
+	require.NoError(t, err)
+
+	c := NewClientHTTP(ClientOptions{
+		Timeout: 10 * time.Second,
+		URL:     u,
+	})
+
+	in := inputTest{Data: "test"}
+	var out outputTest
+
+	require.NoError(t, c.Call(context.Background(), http.MethodPost, "/test", encoders.FormatJSON, &in, &out))
+
+	assert.Equal(t, "test", out.Result)
+}
