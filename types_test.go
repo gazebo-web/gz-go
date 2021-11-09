47d9dec37b357a430743cbc5f79cdb45bc5d34e7
Marcos Huck <marcos.h3012@gmail.com>
2021-11-09 18:51:22 +0000

    Develop to master

---
+++ types_test.go
@@ -0,0 +1,39 @@
+package ign
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInt(t *testing.T) {
+	r := Int(1)
+	assert.Equal(t, 1, *r)
+}
+
+func TestInt64(t *testing.T) {
+	r := Int64(1)
+	assert.Equal(t, int64(1), *r)
+}
+
+func TestFloat64(t *testing.T) {
+	r := Float64(1.0)
+	assert.Equal(t, 1.0, *r)
+}
+
+func TestString(t *testing.T) {
+	r := String("test")
+	assert.Equal(t, "test", *r)
+}
+
+func TestStringSlice(t *testing.T) {
+	input := []string{"a", "b", "c"}
+	slice := StringSlice(input)
+	for i, s := range slice {
+		assert.Equal(t, input[i], *s)
+	}
+}
+
+func TestBool(t *testing.T) {
+	r := Bool(true)
+	assert.Equal(t, true, *r)
+}
