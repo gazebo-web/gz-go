515a4f9a3fc3fb6e36bbaf9214281c650170b3e0
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-03-23 17:28:24 -0300

    Change GetID return type

--- repository/model.go
+++ repository/model.go
@@ -2,8 +2,8 @@ package repository
 
 // Model represents a generic entity. A Model is part of the domain layer and is  persisted by a certain Repository.
 type Model interface {
-	// GetID returns the ID of the model that has been persisted. It returns -1 if no value has been defined.
-	GetID() int
+	// GetID returns the ID of the model that has been persisted. It returns 0 if no value has been defined.
+	GetID() uint
 	// TableName returns the table/collection name for a certain model.
 	TableName() string
 }
