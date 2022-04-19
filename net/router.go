e5cc0e4dd3fa5c6acbc82a05245fb2fe8a59a274
Marcos Huck <marcos.h3012@gmail.com>
2022-04-19 19:03:17 +0000

    Import go-chi V5

--- net/router.go
+++ net/router.go
@@ -1,8 +1,8 @@
 package net
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 	"net/http"
 )
