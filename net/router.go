0fdb0ed19b6c95e8f29ff28dc5207a0f55e289d0
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-04-26 15:04:12 -0300

    Add middleware injection
    Middleware injection allow developers injecting a set of custom middlewares to their routers.

--- net/router.go
+++ net/router.go
@@ -1,21 +1,31 @@
 package net
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/render"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
+// DefaultMiddlewares groups a set of middlewares used across different services.
+var DefaultMiddlewares = []Middleware{
+	middleware.RequestID,
+	middleware.RealIP,
+	// TODO: Uncomment this line when solving issue with health-checks.
+	//middleware.Logger,
+	middleware.Recoverer,
+}
+
+// Middleware is function that gets executed before each HTTP handler.
+// It's useful for adding request-specific logic to every request like logging and setting request id.
+type Middleware func(handler http.Handler) http.Handler
+
 // NewRouter initializes a new HTTP router using chi.
 // It also loads middlewares used on every incoming HTTP request.
-func NewRouter() chi.Router {
+func NewRouter(middlewares ...Middleware) chi.Router {
 	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
+	for _, m := range middlewares {
+		r.Use(m)
+	}
 	return r
 }
 
