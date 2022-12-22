30a78f7ec017a150c45a8fee8e7a6439061262e8
Marcos Huck <marcoshuck@ekumenlabs.com>
2022-12-22 11:29:10 -0300

    Remove getEmptyLogger

--- logger.go
+++ logger.go
@@ -81,14 +81,6 @@ func (l *defaultLogImpl) Clone(reqID string) Logger {
 
 var emptyLogger *defaultLogImpl
 
-func getEmptyLogger() Logger {
-	if emptyLogger == nil {
-		l := defaultLogImpl{}
-		emptyLogger = &l
-	}
-	return emptyLogger
-}
-
 // ignLogger - internal implementation for the ign logger interface.
 // The ignLogger will log to terminal and also to rollbar, if configured.
 // The ignLogger will prefix all logs with a configured request Id.
