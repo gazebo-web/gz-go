9c70a2f4575df1a65b311a0d317bc59b3d5280dd
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-01-31 15:55:49 -0300

    Fix docs

--- storage/storage.go
+++ storage/storage.go
@@ -6,12 +6,12 @@ import "context"
 type Storage interface {
 	// GetFile returns the content of file from a given path.
 	GetFile(ctx context.Context, resource Resource, path string) ([]byte, error)
-	// Download returns the URL where to download the given resource.
+	// Download returns the URL where to download the given resource from.
 	Download(ctx context.Context, resource Resource) (string, error)
 	// Create creates all the directories needed to hold new assets for future resource versions of the identified
 	// uuid and of the given kind.
 	Create(ctx context.Context, owner string, kind Kind, uuid string) error
 	// Upload uploads assets located in the given source folder and placed them into the given resource.
-	// It most be called after Create.
+	// It must be called after Create.
 	Upload(ctx context.Context, resource Resource, source string) error
 }
