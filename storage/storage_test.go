d6334943f0f297bbfe973335d6acdbd10361c1a4
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-02-02 14:48:06 -0300

    Add test cases for generic storage functions

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -1,8 +1,14 @@
 package storage
 
 import (
+	"context"
 	s3api "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -52,4 +58,59 @@ func (suite *StorageTestSuite) TestNewGCSStorage() {
 func (suite *StorageTestSuite) TestNewFilesystemStorage() {
 	storage := newFilesystemStorage("./testdata")
 	suite.Assert().Implements((*Storage)(nil), storage)
+}
+
+func TestUploadDir(t *testing.T) {
+	ctx := context.Background()
+	r := validResource
+	src := "./testdata/example"
+	var paths []string
+	assert.NoError(t, UploadDir(ctx, r, src, func(ctx context.Context, path string, body io.Reader) error {
+		paths = append(paths, path)
+		return nil
+	}))
+	assert.Len(t, paths, 4)
+	assert.Contains(t, paths, "meshes/turtle.dae")
+	assert.Contains(t, paths, "thumbnails/1.png")
+	assert.Contains(t, paths, "model.config")
+	assert.Contains(t, paths, "model.sdf")
+}
+
+func TestReadFile(t *testing.T) {
+	ctx := context.Background()
+	r := validResource
+	path := filepath.Join(t.TempDir(), "test.txt")
+	f, err := os.Create(path)
+	require.NoError(t, err)
+	n, err := io.WriteString(f, "test")
+	require.Equal(t, 4, n)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+
+	b, err := ReadFile(ctx, r, path, func(ctx context.Context, resource Resource, path string) (io.ReadCloser, error) {
+		open, err := os.Open(path)
+		if err != nil {
+			return nil, err
+		}
+		return open, nil
+	})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, b)
+	assert.Equal(t, "test", string(b))
+}
+
+func TestUploadZip(t *testing.T) {
+	ctx := context.Background()
+	r := validResource
+	location := getZipLocation("./testdata", r)
+	f, err := os.Open(location)
+	require.NoError(t, err)
+
+	var content []byte
+	assert.NoError(t, UploadZip(ctx, r, f, func(ctx context.Context, p string, body io.Reader) error {
+		var err error
+		content, err = io.ReadAll(body)
+		return err
+	}))
+	assert.NotEmpty(t, content)
 }
