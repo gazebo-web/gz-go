dd7babbc0d6b321466c49d567f3f49bbb4dcd62e
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-05-23 17:29:46 -0300

    Update firestore values in Model

--- repository/firestore/model.go
+++ repository/firestore/model.go
@@ -7,12 +7,12 @@ import "time"
 // To use it, embed it in your application-specific repository.Model implementation.
 type Model struct {
 	// CreatedAt contains the date and time at which this model has been persisted.
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `firestore:"created_at"`
 	// UpdatedAt contains the last date and time when this model has been updated.
-	UpdatedAt time.Time `json:"updated_at"`
+	UpdatedAt time.Time `firestore:"updated_at"`
 	// DeletedAt is used to implement soft record deletion. If set, the record will be considered
 	// as deleted.
-	DeletedAt *time.Time `json:"deleted_at" sql:"index"`
+	DeletedAt *time.Time `firestore:"deleted_at"`
 }
 
 // GetID returns the unique identifier for this Model.
