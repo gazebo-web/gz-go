582de905cd149bb0cababbce9e7763403cba553a
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-06-06 10:43:17 -0300

    Move test cases

--- pagination/helpers_test.go
+++ pagination/helpers_test.go
@@ -3,7 +3,6 @@ package pagination
 import (
 	"encoding/base64"
 	"errors"
-	"github.com/gazebo-web/gz-go/v7/repository"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -111,49 +110,6 @@ func TestGetNextPageToken(t *testing.T) {
 
 	now := time.Now()
 	assert.NotEmpty(t, GetNextPageTokenFromTime(now))
-}
-
-func TestSetMaxResults(t *testing.T) {
-	var opts []repository.Option
-
-	opts, err := setMaxResults(opts, nil)
-	assert.NoError(t, err)
-	assert.Len(t, opts, 1)
-}
-
-func TestSetMaxResults_Empty(t *testing.T) {
-	var opts []repository.Option
-
-	req := TestPaginationRequest{}
-
-	opts, err := setMaxResults(opts, &req)
-	assert.NoError(t, err)
-	assert.Len(t, opts, 1)
-}
-
-func TestSetMaxResults_Max(t *testing.T) {
-	var opts []repository.Option
-
-	req := TestPaginationRequest{
-		size: 1001,
-	}
-
-	opts, err := setMaxResults(opts, &req)
-	assert.NoError(t, err)
-	assert.Len(t, opts, 1)
-}
-
-func TestSetMaxResults_Invalid(t *testing.T) {
-	var opts []repository.Option
-
-	req := TestPaginationRequest{
-		size: -100,
-	}
-
-	var err error
-	opts, err = setMaxResults(opts, &req)
-	assert.Error(t, err)
-	assert.Len(t, opts, 0)
 }
 
 func TestGetListAndCursor(t *testing.T) {
