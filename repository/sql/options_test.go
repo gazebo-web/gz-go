1be4697283e3c1460532faffb3bc0e1630e318e4
Marcos Huck <marcoshuck@ekumenlabs.com>
2023-06-26 17:15:50 -0300

    Revert change

--- repository/sql/options_test.go
+++ repository/sql/options_test.go
@@ -159,9 +159,9 @@ func (s *SQLOptionsTestSuite) TestFindOffsetOption() {
 	s.Require().NoError(s.repository.Find(&out, MaxResults(10), Offset(7), Offset(5)))
 	s.Assert().EqualValues([]int{6, 7, 8, 9, 10}, s.getValues(out))
 
-	// Using offset without max results does change the offset
+	// Using offset without max results does not change the offset
 	s.Require().NoError(s.repository.Find(&out, Offset(5)))
-	s.Assert().EqualValues([]int{6, 7, 8, 9, 10}, s.getValues(out))
+	s.Assert().EqualValues([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, s.getValues(out))
 }
 
 // func (s *SQLOptionsTestSuite) TestFindSelectAndGroupByOptions() {
