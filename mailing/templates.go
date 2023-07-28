289af4e87d050044c97d7e0865c6ecf4c18a6970
Marcos Huck <marcoshuck@users.noreply.github.com>
2023-07-28 14:17:50 -0300

    Fix missing rename (#76)
    * Add template wrapper
    
    * Address comments from code review

--- mailing/templates.go
+++ mailing/templates.go
@@ -8,15 +8,15 @@ import (
 // ErrTemplateNotFound is returned when a given template is not found inside the list of available templates.
 var ErrTemplateNotFound = errors.New("template not found")
 
-// templateWrapper implements Sender allowing the template management to be handled by an inner map between
+// templateSender implements Sender allowing the template management to be handled by an inner map between
 // a key and a template location.
-type templateWrapper struct {
+type templateSender struct {
 	templates map[string]string
 	sender    Sender
 }
 
 // Send validates that the given template is contained inside the list of available templates and sends the email.
-func (t *templateWrapper) Send(ctx context.Context, sender string, recipients, cc, bcc []string, subject, template string, data any) error {
+func (t *templateSender) Send(ctx context.Context, sender string, recipients, cc, bcc []string, subject, template string, data any) error {
 	value, ok := t.templates[template]
 	if !ok {
 		return ErrTemplateNotFound
@@ -24,15 +24,19 @@ func (t *templateWrapper) Send(ctx context.Context, sender string, recipients, c
 	return t.sender.Send(ctx, sender, recipients, cc, bcc, subject, value, data)
 }
 
-// NewTemplateWrapper initializes a new Sender implementation that conveniently handles templates by configuring
-// the different available email templates in the given templates argument. It relies on the given sender to actually
-// send the email. This wrapper can be used alongside the AWS SES and Sendgrid implementations.
+// NewTemplateSender initializes a new Sender implementation that sends pre-defined templates using another email
+// sender. Emails are sent by specifying the template to use and providing data for them.
 //
-//	templates: map[string]string{
-//		"event.users.signup": "./templates/users/signup.gohtml",
-//	}
-func NewTemplateWrapper(sender Sender, templates map[string]string) Sender {
-	return &templateWrapper{
+// The sender argument contains a Sender implementation. This is what is actually used to send emails.
+//
+// The templates argument maps template identifiers to template filepaths. The identifiers are used to select the
+// template to send.
+//
+//	Example:
+//	sender := NewSendgridEmailSender(..)
+//	sender = NewTemplateSender(sender, map[string]string{ "event.users.signup": "./templates/users/signup.gohtml", })
+func NewTemplateSender(sender Sender, templates map[string]string) Sender {
+	return &templateSender{
 		sender:    sender,
 		templates: templates,
 	}
