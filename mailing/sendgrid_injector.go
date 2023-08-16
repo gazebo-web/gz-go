824cea236bb2f7c04a77c7d8e030a0683e80f0c8
Marcos Huck <marcoshuck@users.noreply.github.com>
2023-08-16 13:28:57 -0300

    Apply suggestions from code review
    Co-authored-by: Alejo Carballude <alejocarballude@gmail.com>
    Signed-off-by: Marcos Huck <marcoshuck@users.noreply.github.com>

--- mailing/sendgrid_injector.go
+++ mailing/sendgrid_injector.go
@@ -2,15 +2,19 @@ package mailing
 
 import "github.com/gazebo-web/gz-go/v8"
 
-// contentInjector defines the function signature that injects content injection into sendgridEmailBuilder
+// contentInjector is a function that prepares and configures email content to be sent by sendgridEmailService.
 type contentInjector func(b sendgridEmailBuilder, key string, data any) (sendgridEmailBuilder, error)
 
-// injectTemplateContent injects the information needed to send a Sendgrid email using dynamic templates.
+// injectTemplateContent configures a SendGrid email to send a dynamic template with the provided data.
+// key is the unique SendGrid template identifier.
+// data is a map[string]any containing data for template fields.
 func injectTemplateContent(b sendgridEmailBuilder, id string, data any) (sendgridEmailBuilder, error) {
 	return b.Template(id, data), nil
 }
 
-// injectHTMLContent injects the actual content after parsing an HTML Go template.
+// injectHTMLContent configures a SendGrid email to send a rendered HTML Go template.
+// path contains the path to the Go template to render.
+// data contains values for template fields.
 func injectHTMLContent(b sendgridEmailBuilder, path string, data any) (sendgridEmailBuilder, error) {
 	content, err := gz.ParseHTMLTemplate(path, data)
 	if err != nil {
