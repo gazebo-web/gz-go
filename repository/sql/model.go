0de951a8e32870b93bc681dbde7d2eb08966343c
Marcos Huck <huckm@google.com>
2023-12-07 20:53:09 +0000

    Ignore SQL fields with firestore

--- repository/sql/model.go
+++ repository/sql/model.go
@@ -7,14 +7,14 @@ import "time"
 // To use it, embed it in your application-specific repository.Model implementation.
 type Model struct {
 	// ID contains the primary key identifier.
-	ID uint `gorm:"primaryKey"`
+	ID uint `gorm:"primaryKey" firestore:"-"`
 	// CreatedAt contains the date and time at which this model has been persisted.
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"created_at" firestore:"-"`
 	// UpdatedAt contains the last date and time when this model has been updated.
-	UpdatedAt time.Time `json:"updated_at"`
+	UpdatedAt time.Time `json:"updated_at" firestore:"-"`
 	// DeletedAt is used to implement soft record deletion. If set, the record will be considered
 	// as deleted.
-	DeletedAt *time.Time `json:"deleted_at" gorm:"index"`
+	DeletedAt *time.Time `json:"deleted_at" gorm:"index" firestore:"-"`
 }
 
 // GetID returns the unique identifier for this Model.
